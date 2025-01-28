89ca469cfd5cf7c65e0c4252cbed5fa3c36ef292
daisy8112 <munisa_shayzoqova8112@mail.ru>
2025-01-28 14:45:02 +0500

    Birinchi commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const emailInput = document.querySelector('.email-input');
+    const signupButton = document.querySelector('.signup-button');
+    const message = document.querySelector('#message');
+  
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  
+    emailInput.addEventListener('input', function() {
+      if (emailPattern.test(emailInput.value)) {
+        emailInput.style.borderColor = 'rgb(232,240,254)'; 
+        emailInput.style.backgroundColor = '#fff'; 
+        emailInput.style.color = '#000'; 
+        message.style.display = 'none'; 
+      } else {
+        emailInput.style.borderColor = 'red'; 
+        emailInput.style.backgroundColor = '#fff'; 
+        emailInput.style.color = 'red'; 
+        message.style.display = 'block'; 
+      }
+    });
+  
+    signupButton.addEventListener('click', function(event) {
+      if (!emailPattern.test(emailInput.value)) {
+        event.preventDefault(); 
+      }
+    });
+});
